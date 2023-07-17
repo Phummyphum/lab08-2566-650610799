9b1b7da7138d2ca811af494f02a6231be113f9d6
Phummy_phum <phumrapee.phum@gmail.com>
2023-07-17 16:40:29 +0700

    complete

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,4 @@
-import { Header } from "../../components/header";
+import { Header } from "../../components/Header";
 import { TaskInput } from "../../components/TaskInput";
 import { Task } from "../../components/Task";
 import { Footer } from "../../components/Footer";
